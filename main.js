e8bac6bd7b7a1c7ba9478d6f4f5cf96cee75d51a
Grace Shih <vocalise@gmail.com>
2020-01-14 06:47:09 +0000

    Done.

--- main.js
+++ main.js
@@ -5,6 +5,39 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+document.addEventListener("DOMContentLoaded", function(event) {
+  let modalDivElement = document.getElementById("modal");
+  modalDivElement.setAttribute("class", "hidden");
+  function monitorLikes() {
+    let likes = document.querySelectorAll(".like-glyph");
+    for (let i = 0; i < likes.length; i++) {
+      likes[i].addEventListener("click", (event) => {
+        event.preventDefault();
+        mimicServerCall()
+        .then(function(response) {
+          if (likes[i].textContent == EMPTY_HEART) {          
+            likes[i].textContent = FULL_HEART;
+            likes[i].setAttribute("class", "activated-heart")
+          } else {
+            likes[i].textContent = EMPTY_HEART;
+            likes[i].removeAttribute("class", "activated-heart")
+          }          
+        })
+        .catch(function(error) {
+          modalDivElement.removeAttribute("class", "hidden");
+          modalMsgElement = document.getElementById("modal-message");
+          modalMsgElement.textContent = error;
+          hideError();
+        }); //end catch
+      }) // end addEventListener
+    } // end for
+  }//end function
+  
+  function hideError() {
+    setTimeout(function() { modalDivElement.setAttribute("class", "hidden")}, 5000);
+  }
+  monitorLikes();
+})
 
 
 //------------------------------------------------------------------------------
